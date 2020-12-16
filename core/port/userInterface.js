0f23332f0e1c182a93b7b417a8fd0906a16e5aae
noydono <th.gabillard@laposte.net>
2020-12-16 09:36:47 +0100

    commentaire userInterface

--- core/port/userInterface.js
+++ core/port/userInterface.js
@@ -1,7 +1,7 @@
 'use strict';
 // cet interface me sers de prise pour branché mes dataprovider pour ne pas dépendre d'eux mais l'inverse
 // je comprend l'intêret, le pourquois mais pas le comment. 
-// par exemple je peut suprimer une method de ce fichier masi mon test passera toujour et 
+// par exemple je peut suprimer une method de ce fichier mais mon test passera toujour et 
 // si je supprime la method dans le repository le throw error renvoie rien.
 // est ce que a cause du mock simule une fonction donc ne l'appel pas vraiemnt alors l'interface n'est pas réellement testé ?
 module.exports = class {
